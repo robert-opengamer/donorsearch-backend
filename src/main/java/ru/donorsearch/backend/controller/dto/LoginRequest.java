ec08719e17d46300c9a6f0dac50350932afe5907
wwerlosh <igor.shverlo@yandex.ru>
2024-02-24 00:43:28 +0300

    last fixes

--- src/main/java/ru/donorsearch/backend/controller/dto/LoginRequest.java
+++ src/main/java/ru/donorsearch/backend/controller/dto/LoginRequest.java
@@ -1,13 +1,17 @@
 package ru.donorsearch.backend.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class LoginRequest {
     private String username;
-
     private String password;
+    @JsonProperty("chat_id")
+    private long chatId;
 
-    public LoginRequest(String username, String password) {
+    public LoginRequest(String username, String password, long chatId) {
         this.username = username;
         this.password = password;
+        this.chatId = chatId;
     }
 
     public LoginRequest() {
@@ -27,5 +31,13 @@ public class LoginRequest {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
     }
 }
