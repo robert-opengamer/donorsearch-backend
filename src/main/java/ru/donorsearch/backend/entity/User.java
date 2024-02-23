5a931d0c92fee5b606884e1c8134e7b9fae1a969
wwerlosh <igor.shverlo@yandex.ru>
2024-02-23 18:19:40 +0300

    fix

--- src/main/java/ru/donorsearch/backend/entity/User.java
+++ src/main/java/ru/donorsearch/backend/entity/User.java
@@ -2,8 +2,10 @@ package ru.donorsearch.backend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     private Long id;
