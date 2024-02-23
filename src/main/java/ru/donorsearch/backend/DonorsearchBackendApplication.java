6fbcdd9aa8c3fcde4bc731156044a6038705c6e8
wwerlosh <igor.shverlo@yandex.ru>
2024-02-23 16:04:58 +0300

    init

---
+++ src/main/java/ru/donorsearch/backend/DonorsearchBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.donorsearch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonorsearchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonorsearchBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/donorsearch/backend/DonorsearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.donorsearch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonorsearchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
