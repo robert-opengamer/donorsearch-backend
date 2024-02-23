364b4d580835595482c90e7ff13b5041db5e8ae4
wwerlosh <igor.shverlo@yandex.ru>
2024-02-23 19:30:51 +0300

    added returned token in cookie

--- src/main/java/ru/donorsearch/backend/service/AuthService.java
+++ src/main/java/ru/donorsearch/backend/service/AuthService.java
@@ -2,6 +2,9 @@ package ru.donorsearch.backend.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.Cookie;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -40,8 +43,13 @@ public class AuthService {
 
     public ResponseEntity<LoginResponse> login(LoginRequest request) throws UnsupportedEncodingException, JsonProcessingException {
         String token = authHttpClient.loginClient(request);
+
+        Duration cookieDuration = Duration.ofHours(1);
+        ZonedDateTime expiresDateTime = ZonedDateTime.now().plus(cookieDuration);
+        String expiresFormatted = expiresDateTime.format(DateTimeFormatter.RFC_1123_DATE_TIME);
+
         HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.SET_COOKIE, "token=" + token);
+        headers.add(HttpHeaders.SET_COOKIE, String.format("token=%s; expires=%s", token, expiresFormatted));
 
         return new ResponseEntity<>(new LoginResponse(token), headers, HttpStatus.OK);
     }
